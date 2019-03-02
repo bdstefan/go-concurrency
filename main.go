e9dbe2de78bb6b2c684ef5852329b5fa8043ae61
BDS <dumitru.boila@gmail.com>
2019-03-02 22:30:26 +0200

    Create a blocking channel and a go routine that still performs an HTTP GET request

--- main.go
+++ main.go
@@ -12,17 +12,24 @@ func  main()  {
 		"https://www.stackoverflow.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<- c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 
 	if err != nil {
 		fmt.Println("Failed to perform GET request on", link)
+		c <- "It might be down"
+		return
 	}
 
 	fmt.Println(link, "is up")
+	c <- "It is up for sure"
 }
